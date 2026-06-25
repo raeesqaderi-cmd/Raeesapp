99e2206f4d982f7462247781407ab41dfa022ac6
Raeestalk <raeesqaderi@gmail.com>
2026-06-25 14:43:01 +0330

    Remove KSP plugin from build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.google.devtools.ksp)
     alias(libs.plugins.roborazzi)
     alias(libs.plugins.secrets)
 }
